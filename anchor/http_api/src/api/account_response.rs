0ec81589dfad68c0a992ec352bd2f7cc1a2d4232
Anton Hughes <kurrent93@gmail.com>
2025-01-10 17:37:01 +1300

    acct response types

---
+++ anchor/http_api/src/api/account_response.rs
@@ -0,0 +1,51 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+pub struct AccountsResponse {
+    #[serde(rename = "type")]
+    accounts_response_type: String,
+
+    filter: Filter,
+
+    data: Vec<Account>,
+}
+
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Account {
+    id: i64,
+
+    owner_address: String,
+
+    recipient_address: Option<String>,
+
+    network: Network,
+
+    version: Version,
+}
+
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Network {
+    Mainnet,
+    Testnet,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct NetworkParams {
+    network: Network,
+}
+
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Version {
+    V4,
+}
+
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Filter {
+    page: i64,
+
+    per_page: i64,
+}
